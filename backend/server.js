974339fba1defe2c04494bc9ad7dfd29feeb18ba
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-03-13 19:12:09 +0530

    Added cors support

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 const tasksRoute = require('./routes/tasksRoute')
 
