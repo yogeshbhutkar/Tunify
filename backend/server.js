8c460446c85273ae0ff58cd129c81982995ccbf3
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-03-16 09:49:53 +0530

    Updated CORS

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,7 @@ const app = express()
 
 app.use(cors())
 
-var corsOptions = {
-    origin: 'https://tunify-yogeshbhutkar.vercel.app/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+
 
 const tasksRoute = require('./routes/tasksRoute')
 
@@ -20,11 +17,11 @@ app.use((req, res, next)=>{
     next()
 })
 
-app.get('/',  cors(corsOptions), (req, res)=>{
+app.get('/',   (req, res)=>{
     return res.json({home: "this is the home route"})
 })
 
-app.use('/api/tasks/',  cors(corsOptions), tasksRoute)
+app.use('/api/tasks/',  tasksRoute)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>{
